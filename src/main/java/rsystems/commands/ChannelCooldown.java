2001ec95608c16cf30bf2f8b7b53937dfa3b03ff
Matt <Blade2021@live.com>
2020-10-18 14:52:56 -0400

    Fix ChannelCooldown bug

--- src/main/java/rsystems/commands/ChannelCooldown.java
+++ src/main/java/rsystems/commands/ChannelCooldown.java
@@ -9,9 +9,11 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import rsystems.SherlockBot;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.Character.isDigit;
 import static rsystems.SherlockBot.database;
 
 public class ChannelCooldown extends ListenerAdapter {
@@ -21,25 +23,75 @@ public class ChannelCooldown extends ListenerAdapter {
             return;
         }
 
-        if (SherlockBot.commands.get(13).checkCommandMod(event.getMessage(),2)) {
+        if (SherlockBot.commands.get(13).checkCommandMod(event.getMessage(), 2)) {
 
             String[] args = event.getMessage().getContentRaw().split("\\s+");
 
-            if((args.length > 1) && (args[1].equalsIgnoreCase("reset"))){
-                endChannelCooldown(event.getGuild(), event.getChannel());
+            if ((args.length > 1) && (args[1].equalsIgnoreCase("reset"))) {
 
-                if (event.getChannel().getName().contains("\uD83E\uDD76")) {
-                    int lastFlakeLocation = event.getChannel().getName().lastIndexOf("\uD83E\uDD76");
-                    event.getChannel().getManager().setName(event.getChannel().getName().substring(0, lastFlakeLocation)).queueAfter(10,TimeUnit.SECONDS);
+                /*
+                Attempt to remove cooldown from channel if a cooldown was found on defined channel.
+                 */
+                if (endChannelCooldown(event.getGuild(), event.getChannel())) {
+                    if (event.getChannel().getName().contains("\uD83E\uDD76")) {
+                        int lastFlakeLocation = event.getChannel().getName().lastIndexOf("\uD83E\uDD76");
+                        event.getChannel().getManager().setName(event.getChannel().getName().substring(0, lastFlakeLocation)).queueAfter(10, TimeUnit.SECONDS);
+                    }
+                    event.getMessage().addReaction("\uD83D\uDE07").queue();
                 }
 
-                event.getMessage().addReaction("\uD83D\uDE07").queue();
-
-                return;
             } else {
 
+                LocalDateTime expireDateTime = LocalDateTime.now().plusHours(1);
+
+                if (event.getMessage().getContentDisplay().contains("-t ")) {
+
+                    StringBuilder numberString = new StringBuilder();
+                    //Parse the numberString into a integer
+                    int number = 0;
+                    String chronoType = "";
+
+                    //Grab the string AFTER -t, excluding the first 3 characters (Length of -t )
+                    String timeArgument = event.getMessage().getContentDisplay().substring(event.getMessage().getContentDisplay().indexOf("-t ") + 3);
+                    char chronoUnit = 0;
+
+
+                    // Parse through the string character by character until a digit is not found, then move the char for processing
+                    for (Character c : timeArgument.toCharArray()) {
+                        if (isDigit(c)) {
+                            numberString.append(c);
+                        } else if(c == ' '){
+                            // do nothing (skip spaces)
+                        } else {
+                            chronoUnit = c;
+                            break;
+                        }
+                    }
+                    //Parse the numberString into a integer for incrementing the date
+                    number = Integer.parseInt(numberString.toString());
+
+                    //See what type of unit to use
+                    switch (chronoUnit) {
+                        case 'm':
+                        case 'M':
+                            expireDateTime = expireDateTime.plus(number, ChronoUnit.MINUTES);
+                            break;
+                        case 'h':
+                        case 'H':
+                            expireDateTime = expireDateTime.plus(number, ChronoUnit.HOURS);
+                            break;
+                        case 'd':
+                        case 'D':
+                            expireDateTime = expireDateTime.plus(number, ChronoUnit.DAYS);
+                            break;
+                        default:
+                            event.getChannel().sendMessage("Incorrect command formatting used").queue();
+                            return;
+                    }
+                }
+
                 event.getChannel().sendMessage("This channel is being put on a temporary cooldown.  Please use other channels.").queue();
-                startChannelCooldown(event.getGuild(), event.getChannel(), 1, 'h');
+                startChannelCooldown(event.getGuild(), event.getChannel(), expireDateTime);
 
                 event.getChannel().getManager().setName(event.getChannel().getName() + "\uD83E\uDD76").queueAfter(10, TimeUnit.SECONDS);
             }
@@ -47,20 +99,31 @@ public class ChannelCooldown extends ListenerAdapter {
         }
     }
 
-    private void startChannelCooldown(Guild guild, TextChannel channel, int timeValue, char timeType) {
+    private void startChannelCooldown(Guild guild, TextChannel channel, LocalDateTime expirationDate) {
         System.out.println("Starting cooldown sequence");
 
         LocalDateTime currentDateTime = LocalDateTime.now();
-        LocalDateTime expireDateTime = currentDateTime.plusHours(1);
 
-        if(!channel.getPermissionOverrides().contains(guild.getPublicRole())){
-            channel.createPermissionOverride(guild.getPublicRole()).setDeny(Permission.MESSAGE_WRITE).queue();
-            database.insertTimedEvent(guild.getIdLong(), channel.getIdLong(), 2, "Channel Cooldown", guild.getPublicRole().getIdLong(), 0, currentDateTime, expireDateTime);
+
+        /*
+        if (!channel.getRolePermissionOverrides().contains(guild.getPublicRole())) {
+            try{
+                channel.createPermissionOverride(guild.getPublicRole()).setDeny(Permission.MESSAGE_WRITE).queue();
+                database.insertTimedEvent(guild.getIdLong(), channel.getIdLong(), 2, "Channel Cooldown", guild.getPublicRole().getIdLong(), 0, currentDateTime, expirationDate);
+            } catch(IllegalStateException e){
+                System.out.println("Could not create override for " + guild.getPublicRole());
+            }
         }
 
-        for (PermissionOverride permissionOverride : channel.getPermissionOverrides()) {
-            if (permissionOverride.isRoleOverride()) {
+         */
+
+
+        for (PermissionOverride permissionOverride : channel.getRolePermissionOverrides()) {
                 int currentState = 0;
+
+                if(permissionOverride.getRole().isPublicRole()){
+                    System.out.println("debug");
+                }
 
                 if (permissionOverride.getAllowed().contains(Permission.MESSAGE_WRITE)) {
                     currentState = 1;
@@ -69,31 +132,35 @@ public class ChannelCooldown extends ListenerAdapter {
                 }
 
                 System.out.println("Attempting to set override for Role:" + permissionOverride.getRole().getName());
-                database.insertTimedEvent(guild.getIdLong(), channel.getIdLong(), 2, "Channel Cooldown", permissionOverride.getRole().getIdLong(), currentState, currentDateTime, expireDateTime);
-                channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).setDeny(Permission.MESSAGE_WRITE).queue();
-            }
+                database.insertTimedEvent(guild.getIdLong(), channel.getIdLong(), 2, "Channel Cooldown", permissionOverride.getRole().getIdLong(), currentState, currentDateTime, expirationDate);
+                channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).deny(Permission.MESSAGE_WRITE).queue();
         }
     }
 
-    private void endChannelCooldown(Guild guild, TextChannel channel) {
+    public boolean endChannelCooldown(Guild guild, TextChannel channel) {
         System.out.println("Resetting cooldown for Channel");
         HashMap<Long, Integer> eventMap = new HashMap<>();
         eventMap.putAll(database.removeCooldown(guild.getIdLong(), channel.getIdLong()));
-        for (PermissionOverride permissionOverride : channel.getPermissionOverrides()) {
-            if (permissionOverride.isRoleOverride()) {
-                System.out.println("Attempting to reset override for Role:" + permissionOverride.getRole().getName());
-                try {
-                    if (eventMap.get(permissionOverride.getIdLong()) == 1) {
-                        channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).setAllow(Permission.MESSAGE_WRITE).queue();
-                    } else if (eventMap.get(permissionOverride.getIdLong()) == 2) {
-                        channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).setDeny(Permission.MESSAGE_WRITE).queue();
-                    } else {
-                        channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).clear(Permission.MESSAGE_WRITE).queue();
+        if (eventMap.size() > 0) {
+            System.out.println("debug size:" + eventMap.size());
+            for (PermissionOverride permissionOverride : channel.getPermissionOverrides()) {
+                if (permissionOverride.isRoleOverride()) {
+                    System.out.println("Attempting to reset override for Role:" + permissionOverride.getRole().getName());
+                    try {
+                        if (eventMap.get(permissionOverride.getIdLong()) == 1) {
+                            channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).grant(Permission.MESSAGE_WRITE).queue();
+                        } else if (eventMap.get(permissionOverride.getIdLong()) == 2) {
+                            channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).deny(Permission.MESSAGE_WRITE).queue();
+                        } else {
+                            channel.upsertPermissionOverride(permissionOverride.getPermissionHolder()).clear(Permission.MESSAGE_WRITE).queue();
+                        }
+                    } catch (NullPointerException e) {
+                        System.out.println("Did not find a permission override for ID: " + permissionOverride.getId());
                     }
-                } catch (NullPointerException e) {
-                    System.out.println("Did not find a permission override for ID: " + permissionOverride.getId());
                 }
             }
+            return true;
         }
+        return false;
     }
 }
