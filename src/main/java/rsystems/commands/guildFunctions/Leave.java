5075f08dffd054a4d90a01536a5c0e40abfd8e85
Matt <Blade2021@live.com>
2021-11-05 16:02:04 -0400

    Leave command

--- src/main/java/rsystems/commands/guildFunctions/Leave.java
+++ src/main/java/rsystems/commands/guildFunctions/Leave.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import rsystems.SherlockBot;
 import rsystems.objects.Command;
 
 import java.sql.SQLException;
@@ -18,6 +19,17 @@ public class Leave extends Command {
     @Override
     public void dispatch(User sender, MessageChannel channel, Message message, String content, GuildMessageReceivedEvent event) throws SQLException {
 
+        Long muteRoleID = SherlockBot.guildMap.get(event.getGuild().getIdLong()).getMuteRoleID();
+
+        if ((muteRoleID != null) && (event.getGuild().getRoleById(muteRoleID) != null)){
+
+            event.getGuild().getRoleById(muteRoleID).delete().reason("Leaving guild.  Removing auto-created mute role").queue(Success -> {
+                event.getGuild().leave().queue();
+            });
+
+        } else {
+            event.getGuild().leave().queue();
+        }
     }
 
     @Override
