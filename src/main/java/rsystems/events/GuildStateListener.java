6f93a1860d0ae8b16d9545255a795ace9896ad4a
Matt <Blade2021@live.com>
2021-11-05 16:19:59 -0400

    Join/Leave States

---
+++ src/main/java/rsystems/events/GuildStateListener.java
@@ -0,0 +1,93 @@
+package rsystems.events;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Category;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.api.events.guild.UnavailableGuildLeaveEvent;
+import net.dv8tion.jda.api.exceptions.PermissionException;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import rsystems.SherlockBot;
+import rsystems.handlers.ErrorReportHandler;
+import rsystems.objects.GuildSettings;
+
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class GuildStateListener extends ListenerAdapter {
+
+    @Override
+    public void onUnavailableGuildLeave(UnavailableGuildLeaveEvent event) {
+        SherlockBot.database.deleteRow("Guilds","GuildID",event.getGuildIdLong());
+    }
+
+    @Override
+    public void onGuildLeave(GuildLeaveEvent event) {
+        SherlockBot.database.deleteRow("Guilds","GuildID",event.getGuild().getIdLong());
+    }
+
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        try {
+            if(SherlockBot.database.getLong("BlacklistedGuilds","GuildID","GuildID",event.getGuild().getIdLong()) != null){
+                event.getGuild().leave().queue();
+                return;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        SherlockBot.guildMap.putIfAbsent(event.getGuild().getIdLong(),new GuildSettings(event.getGuild().getIdLong()));
+
+        try {
+            if(SherlockBot.database.insertGuild(event.getGuild().getIdLong(),event.getGuild().getOwnerIdLong()) >= 1){
+                event.getGuild().createRole().setColor(Color.decode("#B0A98F")).setName("SL-Muted").queue(role -> {
+                    SherlockBot.database.putValue("Guilds","MuteRoleID","GuildID",event.getGuild().getIdLong(),role.getIdLong());
+
+                    ArrayList<Permission> mutePerms = new ArrayList<>();
+                    mutePerms.add(Permission.MESSAGE_WRITE);
+                    mutePerms.add(Permission.MESSAGE_ADD_REACTION);
+                    mutePerms.add(Permission.VOICE_STREAM);
+                    mutePerms.add(Permission.VOICE_SPEAK);
+                    mutePerms.add(Permission.CREATE_INSTANT_INVITE);
+                    mutePerms.add(Permission.USE_PRIVATE_THREADS);
+                    mutePerms.add(Permission.USE_PUBLIC_THREADS);
+
+                    for(Category category : event.getGuild().getCategories()){
+                        try{
+                            category.createPermissionOverride(role).setDeny(mutePerms).reason("Initiating bot perms").queue();
+                        } catch (PermissionException e){
+                            break;
+                        }
+                    }
+
+                    //Set the mute role permission override for each channel
+                    for (TextChannel channel : event.getGuild().getTextChannels()) {
+                        try {
+                            channel.createPermissionOverride(role).setDeny(mutePerms).queue();
+                        } catch (PermissionException e) {
+                            break;
+                        }
+                    }
+
+                    for (VoiceChannel voiceChannel:event.getGuild().getVoiceChannels()){
+                        try{
+                            voiceChannel.createPermissionOverride(role).setDeny(mutePerms).queue();
+                        } catch(PermissionException e){
+                            break;
+                        }
+                    }
+
+                    SherlockBot.guildMap.get(event.getGuild().getIdLong()).setMuteRoleID(role.getIdLong());
+                });
+            } else {
+                ErrorReportHandler.sendErrorReport("Failed to add guild to database",event.getGuild().getIdLong());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
