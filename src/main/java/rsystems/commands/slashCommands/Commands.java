d1cefd48ff574807c7d5d23dda9dba3f7fdb9010
Matt <Blade2021@live.com>
2021-11-20 08:24:16 -0500

    Commands Slash Command

---
+++ src/main/java/rsystems/commands/slashCommands/Commands.java
@@ -0,0 +1,76 @@
+package rsystems.commands.slashCommands;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import rsystems.SherlockBot;
+import rsystems.handlers.Dispatcher;
+import rsystems.handlers.SlashCommandDispatcher;
+import rsystems.objects.Command;
+import rsystems.objects.SlashCommand;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Commands extends SlashCommand {
+    @Override
+    public boolean isEphemeral() {
+        return true;
+    }
+
+    @Override
+    public void dispatch(User sender, MessageChannel channel, String content, SlashCommandEvent event) {
+        event.deferReply(this.isEphemeral()).queue();
+
+
+        ArrayList<String> commandList = new ArrayList<>();
+        for(Command c: SherlockBot.dispatcher.getCommands()){
+            try {
+                if(Dispatcher.isAuthorized(c,event.getGuild().getIdLong(),event.getMember(),c.getPermissionIndex())){
+                    commandList.add(c.getName());
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ArrayList<String> slashCMDList = new ArrayList<>();
+        for(SlashCommand c: SherlockBot.slashCommandDispatcher.getCommands()){
+            try {
+                if(SlashCommandDispatcher.isAuthorized(c,event.getGuild().getIdLong(),event.getMember(),c.getPermissionIndex())){
+                    slashCMDList.add(c.getName());
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Sort commands into alphabetical order.  Your welcome SG.
+        commandList.sort(Comparator.naturalOrder());
+        slashCMDList.sort(Comparator.naturalOrder());
+
+        if((commandList.size() == 0) && (slashCMDList.size() == 0)){
+            reply(event,"You have no commands available",this.isEphemeral());
+        } else {
+
+            StringBuilder commandsString = new StringBuilder();
+            if(commandList.size() > 0){
+                commandsString.append("`Chat Commands:` \n").append(commandList.toString().replaceFirst("\\[","").replace("]","")).append("\n\n");
+            }
+
+            if(slashCMDList.size() > 0){
+                commandsString.append("`Slash Commands:` \n").append(slashCMDList.toString().replaceFirst("\\[","").replace("]",""));
+            }
+
+            reply(event,commandsString.toString(),this.isEphemeral());
+
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "Return a list of available commands for your user account";
+    }
+
+}
